7aa1a19c3e5aaa6b5d37d220579cb1bd9d1c6be0
davidecaputo <daca9621@gmail.com>
2023-05-17 11:06:59 +0200

    Add elements

--- Movie.php
+++ Movie.php
@@ -1,4 +1,31 @@
 <?php
+class Movie {
+    public $id;
+    public $title;
+    public $original_title;
+    public $nationality;
+    public $date;
+    public $vote;
+    public $image;
 
+    public function __construct($id, $title, $original_title, $nationality, $date, $vote, $image){
+        $this->id = $id;
+        $this->title = $title;
+        $this->original_title = $original_title;
+        $this->nationality = $nationality;
+        $this->date = $date;
+        $this->vote = $vote;
+        $this->image = $image;
+    }
 
+    public function printMovie(){
+        return "
+            <h1>$this->title</h1>
+            <p>$this->nationality</p>
+            <p>$this->date</p>
+            <p>$this->vote</p>
+            <img src='$this->image' alt='$this->title'>
+        ";
+    }
+}
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
+include './Movie.php';
 
+$movie = new Movie(1, 'Il Padrino', 'The Godfather', 'en', '1972/03/24', '9.2', 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/r4gnMXoY1efvaolNDjn3nj4046S.jpg');
+$movie2 = new Movie(2,'Via Col Vento', 'Gone With The Wind', 'en', '1939/12/15', '8.6', 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xRyW60TXvX7Q2HSbpz8nZxKaTkL.jpg');
+$movie3 = new Movie(3, 'Psycho', 'Psycho', 'en', '1960/06/16', '9.4', 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/tdqX0MWaFHuGwUygYn7j6eluOdP.jpg');
+$movie4 = new Movie(4,'Gravity', 'Gravity', 'en', '2013/08/28', '7.8', 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/4pRXqT7wQmoH5sH6Z4WYmbAFG4t.jpg');
 
-?>
+var_dump($movie);
+var_dump($movie2);
+var_dump($movie3);
+var_dump($movie4);
+
+echo $movie->printMovie();
+echo $movie2->printMovie();
+echo $movie3->printMovie();
+echo $movie4->printMovie();
